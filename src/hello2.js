ddd2f6251ef762d824b5c63e22b531cf7bc8dc81
Anu Sihv <anu.sihv@gmail.com>
2023-03-17 15:12:00 +0200

    Add scss support

--- src/hello2.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    hello(message) {
-        console.log('hello!' + message);
-    }
-}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './style.scss';
+
 // import App from './App.svelte'
 
 // const app = new App({
@@ -6,8 +8,3 @@
 
 // export default app
 
-console.log('hello');
-import hello from './hello.js';
-hello.hello('esmodule');
-let hello2 = require('./hello2.js');
-hello2.hello('commonjs');
