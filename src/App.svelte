36ee501ea58666661d934390a5d30e4856ecf0b1
Anu Sihv <anu.sihv@gmail.com>
2023-03-18 13:05:47 +0200

    add ItemList component

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,28 @@
 <script>
-    let counter = 0;
-    let message = 'Hello Svelte';
-    let items = ['piim', 'sai', 'viin', 'õlu']
+    import ItemList from "./components/ItemList.svelte";
 
-    function add(){
-        items.push(message);
+    let counter = 0;
+    let message = '';
+    let items = [
+        {name:'piim', done: false},
+        {name:'sai', done: true},
+        {name:'viin', done: false},
+        {name:'õlu', done: true},
+    ]
+
+    $: doneItems = items.filter(item => item.done);
+    $: toDoItems = items.filter(item => !item.done);
+
+    function add() {
+        items.push({name: message, done: false});
         items = items;
         message = '';
     }
 </script>
-
-<button class="button is-primary" on:click={() => counter++}>Value is {counter}</button>
 <div class="content">
     <input type="text" class="input" bind:value={message}>
     <button class="button is-primary" on:click={add}>Add</button>
-    <h1>{message}</h1>
-    <ul>
-        {#each items as item}
-        <li>{item}</li>
-        {/each}
-    </ul>
+    <ItemList bind:items={items} title="All Items"></ItemList>
+    <ItemList bind:items={doneItems} title="Done Items"></ItemList>
+    <ItemList bind:items={toDoItems} title="Todo Items"></ItemList>
 </div>
---
+++ src/components/ItemList.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let items, title;
+</script>
+
+<h1>{title}</h1>
+    <ul>
+        {#each items as item}
+        <li>
+            {item.name}
+            <input type="checkbox" bind:checked={item.done}>
+        </li>
+        {/each}
+    </ul>
